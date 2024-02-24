2f152a2a760e8907005600ec375f5159a2e9ab91
Ahmed Zaheer <153592631+ahmed-khan033@users.noreply.github.com>
2024-02-24 23:20:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+ let search = document.querySelector('.search-box');
+
+ document.querySelector('#search-icon').onclick = () => {
+     search.classList.toggle('active');
+    //  navbar.classList.remove('active');
+}
+
+
+ 
+
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll'  ,() => {
+    header.classList.toggle('shadow' , window.scrollY >0)
+})
